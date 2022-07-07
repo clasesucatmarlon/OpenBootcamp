8c4825665cec04b64268fc93c027fbb63618bdd7
Marlon GARCIA <clasesucatmarlon@gmail.com>
2022-07-07 09:51:34 -0500

    Entrega ejercicios 4, 5 y 6 - sección 6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import ContactListComponent from "./components/container/ContactListComponent";
 import TaskListComponent from "./components/container/TaskListComponent";
+import Clock  from "./hooks/lifecycle/allCycleFunctional";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <TaskListComponent /> */}
-      <ContactListComponent />
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* <TaskListComponent /> */}
+            <ContactListComponent />
+            <Clock />
+        </div>
+    );
 }
 
 export default App;
