93a9f733971591be790a4b5d31c67b52377aebfb
Marlon GARCIA <clasesucatmarlon@gmail.com>
2022-09-27 19:20:25 -0500

    Sesion-02:FEAT 02 Create Greeting Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Greeting from "./components/pure/Greeting";
+
 function App() {
+    let age = 49;
+    let name = 'Marlon'
     return (
         <div className="App">
             <h1>Aprendiendo reactjs.  Configuración inicial</h1>
+            <Greeting
+                name={name}
+                age={age}
+            />
         </div>
     );
 }
