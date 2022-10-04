056cd54c5bbfbbec256acf4c08a729c3086f320e
marlon garcia <clasesucatmarlon@gmail.com>
2022-10-04 16:41:59 -0500

    Exercises:FEAT 789 Task sesions 7, 8 and 9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import ComponentA from './exercises/sesions_123/ComponentA';
+import ListContacts from './exercises/sesions_789/ListContacts';
 
 function App() {
      return (
         <div className="App">
             <h1 className="bg-dark">Aprendiendo reactjs.  Manejo de Hooks</h1>
-            <ComponentA />
+            <ListContacts />
         </div>
     );
 };
