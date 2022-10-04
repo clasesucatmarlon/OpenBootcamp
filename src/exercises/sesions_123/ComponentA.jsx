6cae4e47cf4211c75b30f7a1a5a60ddc34f04dfa
marlon garcia <clasesucatmarlon@gmail.com>
2022-10-04 08:51:14 -0500

    Exercises:FEAT 123 Task sesions 1, 2 and 3

---
+++ src/exercises/sesions_123/ComponentA.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ComponentB from './ComponentB';
+import { Contact } from './model/contact.class';
+
+const ComponentA = () => {
+    const newContact = new Contact('Marlon', 'García', 'clasesucatmarlon@gmail.com', true);
+    const newContact2 = new Contact('Leidy', 'Gandica', 'lg@gmail.com', false);
+
+    return (
+        <div>
+            <h2>Lista de contactos</h2>
+            <ComponentB newContact={newContact} />
+            <ComponentB newContact={newContact2} />
+        </div>
+    )
+}
+
+export default ComponentA;
