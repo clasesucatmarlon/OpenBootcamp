d4e5a8a4d0a80025151bf2b45a6b899f9b6c14f8
marlon garcia <clasesucatmarlon@gmail.com>
2022-10-19 10:30:00 -0500

    Changed application colors

--- src/components/pure/forms/TaskForm.jsx
+++ src/components/pure/forms/TaskForm.jsx
@@ -82,7 +82,7 @@ const TaskForm = ({ addNewTask, numberTasks }) => {
 				</select>
 			</div>
 			<div>
-				<button type='submit' className='btn btn-primary w-100'>
+				<button type='submit' className='btn text-white w-100'>
 					{numberTasks > 0 ? 'Agregar otra tarea' : 'Crear tarea'}
 				</button>
 			</div>
