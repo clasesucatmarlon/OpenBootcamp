adff94e849e2ba44f35dd0eca0191af1c06b14e0
Marlon GARCIA <clasesucatmarlon@gmail.com>
2022-10-24 15:40:07 -0500

    Sesion-15:REFACTOR 15 Changed banner to header

--- src/components/containers/Header.jsx
+++ src/components/containers/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 //  import logoTask from '../../assets/images/logoTask.png';
-import logoTask22 from '../../assets/images/logoTask22.png';
+import banner from '../../assets/images/banner1.png';
 import '../../assets/styles/header.scss';
 
 const Header = () => {
 	return (
 		<div className='d-flex text-center justify-content-center pt-3'>
 			<div className=''>
-				<img src={logoTask22} alt='Banner' />
+				<img src={banner} alt='Banner' />
 			</div>
 		</div>
 	);
 };
 
 export default Header;
+
