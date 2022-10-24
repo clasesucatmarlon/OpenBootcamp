713ae0a8656677254f185624ec3e0fbf2581a638
Marlon GARCIA <clasesucatmarlon@gmail.com>
2022-10-24 15:47:13 -0500

    Sesion-15:REFACTOR 15 Added shadow to header

--- src/components/containers/Header.jsx
+++ src/components/containers/Header.jsx
@@ -6,12 +6,11 @@ import '../../assets/styles/header.scss';
 const Header = () => {
 	return (
 		<div className='d-flex text-center justify-content-center pt-3'>
-			<div className=''>
-				<img src={banner} alt='Banner' />
+			<div>
+				<img src={banner} alt='Banner' className='shadow' />
 			</div>
 		</div>
 	);
 };
 
 export default Header;
-
